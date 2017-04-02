3d55e67b62004d75d0a5f713c87f428d40078c58
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2017-04-02 22:50:14 +0800

    malloc memory from heap for [1024][10000].

--- CudaDevice/main.cpp
+++ CudaDevice/main.cpp
@@ -8,10 +8,12 @@ extern int vectorsum_main(int argc, char* argv[]);
 
 extern int warp_main(int argc, char* argv[]);
 
+extern int odis_main(int argc, char* argv[]);
+
 int main(int argc, char* argv[])
 {
 	//return demo_main(argc, argv);
 	//return parameters_main(argc, argv);
 	//return vectorsum_main(argc, argv);
-	return warp_main(argc, argv);
+	return odis_main(argc, argv);
 }
